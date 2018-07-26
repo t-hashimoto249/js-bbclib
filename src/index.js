f8562651fe8ad9004d9cad4b7a634bcbae16c7d9
tk-hashimoto <takeshi249@gmail.com>
2018-07-26 12:02:37 +0900

    add index.js in src

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * index.js
+ */
+
+import * as BBcReference from "./bbcclass/BBcReference.js";
+import * as BBcAsset from "./bbcclass/BBcAsset.js";
+import * as BBcTransaction from "./bbcclass/BBcTransaction.js";
+import * as BBcWitness from "./bbcclass/BBcWitness.js";
+import * as BBcEvent from "./bbcclass/BBcEvent.js";
+import * as KeyPair from "./bbcclass/KeyPair.js";
+import * as BBcSignature from "./bbcclass/BBcSignature.js";
+import * as BBcRelation from "./bbcclass/BBcRelation.js";
+import * as BBcCrossRef from "./bbcclass/BBcCrossRef.js";
+import * as BBcPointer from "./bbcclass/BBcPointer.js";
+import * as helper from "./helper.js";
+
+export default { BBcReference, BBcAsset, BBcTransaction, BBcWitness, BBcEvent, KeyPair, BBcSignature, BBcRelation, BBcCrossRef, BBcPointer, helper};
+export { BBcReference, BBcAsset, BBcTransaction, BBcWitness, BBcEvent, KeyPair, BBcSignature, BBcRelation, BBcCrossRef, BBcPointer, helper};
