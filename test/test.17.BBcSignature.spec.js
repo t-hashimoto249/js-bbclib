a27907f6f42150b7e4e67bef4793e4679f14bf46
tk-hashimoto <takeshi249@gmail.com>
2018-12-19 16:51:10 +0900

    update BBcSignature

--- test/test.17.BBcSignature.spec.js
+++ test/test.17.BBcSignature.spec.js
@@ -5,6 +5,7 @@ import * as para from '../src/parameter.js';
 import { Buffer } from 'buffer';
 
 import {getTestEnv} from './prepare.js';
+import jseu from 'js-encoding-utils';
 const env = getTestEnv();
 const bbclib = env.library;
 const envName = env.envName;
@@ -17,26 +18,32 @@ describe(`${envName}: Test BBcSignature`, () => {
     console.log('Test for BBcSignature Class');
 
     const bbcSignature = new bbclib.BBcSignature(para.KeyType.ECDSA_P256v1);
-    bbcSignature.add_signature(new Buffer(8));
-
+    bbcSignature.add_signature(new Uint8Array(8));
     const key_pair = new bbclib.KeyPair();
-
     await key_pair.generate();
 
-    const sig = new Buffer(8);
+    const sig = new Uint8Array(8);
     await bbcSignature.add(sig, key_pair.public_key['_jwk']);
     const signature_serialize = bbcSignature.serialize();
     const signature_deserialize = new bbclib.BBcSignature(2);
     await signature_deserialize.deserialize(signature_serialize);
 
-    //bbcSignature.show_sig();
-    //signature_deserialize.show_sig();
+    console.log("---------");
+    bbcSignature.show_sig();
+    console.log("---------");
+    signature_deserialize.show_sig();
 
-    expect(bbcSignature.signature.toString('hex')).to.equal(signature_deserialize.signature.toString('hex'));
+    expect_uint8Array(bbcSignature.signature,signature_deserialize.signature)
     expect(bbcSignature.pubkey['crv']).to.equal(signature_deserialize.pubkey['crv']);
     expect(bbcSignature.pubkey['EC']).to.equal(signature_deserialize.pubkey['EC']);
     expect(bbcSignature.pubkey['x']).to.equal(signature_deserialize.pubkey['x']);
     expect(bbcSignature.pubkey['y']).to.equal(signature_deserialize.pubkey['y']);
 
   });
+
+
 });
+
+function expect_uint8Array(bin1, bin2){
+  expect(jseu.encoder.arrayBufferToHexString(bin1)).to.be.eq(jseu.encoder.arrayBufferToHexString(bin2));
+}
