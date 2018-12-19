7a71d6f48e468871a8406350458a42d1f449ffa8
Michihito Kato <mkato@zettant.com>
2018-12-19 18:07:21 +0900

    modify pack/unpack for BBcCrossRef

--- test/test.16.BBcCrossRef.spec.js
+++ test/test.16.BBcCrossRef.spec.js
@@ -1,18 +1,18 @@
 import chai from 'chai';
-const expect = chai.expect;
-import { Buffer } from 'buffer';
-
 import {getTestEnv} from './prepare.js';
 import * as helper from '../src/helper';
 import jseu from 'js-encoding-utils';
+const expect = chai.expect;
 const env = getTestEnv();
 const bbclib = env.library;
 const envName = env.envName;
 
 describe(`${envName}: Test BBcCrossRef`, () => {
 
-  it('serialize and deserialize', async () => {
+  it('pack and unpack', async () => {
+    // eslint-disable-next-line no-console
     console.log('***********************');
+    // eslint-disable-next-line no-console
     console.log('Test for BBcCrossRef Class');
 
     const bbcCrossRef = new bbclib.BBcCrossRef(null, null);
@@ -29,9 +29,9 @@ describe(`${envName}: Test BBcCrossRef`, () => {
     bbcCrossRef.set_domain_id(domain_id);
     bbcCrossRef.set_transaction_id(transaction_id);
 
-    const serialize_bbcCrossRef = bbcCrossRef.serialize();
+    const serialize_bbcCrossRef = bbcCrossRef.pack();
     const bbcCrossRef_deserialize = new bbclib.BBcCrossRef(null, null);
-    bbcCrossRef_deserialize.deserialize(serialize_bbcCrossRef);
+    bbcCrossRef_deserialize.unpack(serialize_bbcCrossRef);
 
     expect_uint8Array(bbcCrossRef.domain_id, bbcCrossRef_deserialize.domain_id);
     expect_uint8Array(bbcCrossRef.transaction_id, bbcCrossRef_deserialize.transaction_id);
@@ -42,13 +42,11 @@ describe(`${envName}: Test BBcCrossRef`, () => {
     const crossref_data = helper.fromHexString(crossref_hex_string);
 
     const bbcCrossRef_deserialize = new bbclib.BBcCrossRef(null, null);
-    await bbcCrossRef_deserialize.deserialize(crossref_data);
+    await bbcCrossRef_deserialize.unpack(crossref_data);
 
-    expect(jseu.encoder.arrayBufferToHexString(bbcCrossRef_deserialize.domain_id)).to.be.eq( "16347198acdeed2b6e90715e6f50ba6e8e2728135c7af36aa9903a2b8b834c33" );
-    expect(jseu.encoder.arrayBufferToHexString(bbcCrossRef_deserialize.transaction_id)).to.be.eq( "70bad87b2799a237a1144cc6e898b2a29c3a27c0761cf7b9438d27d4d47a65af" );
-
+    expect(jseu.encoder.arrayBufferToHexString(bbcCrossRef_deserialize.domain_id)).to.be.eq('16347198acdeed2b6e90715e6f50ba6e8e2728135c7af36aa9903a2b8b834c33');
+    expect(jseu.encoder.arrayBufferToHexString(bbcCrossRef_deserialize.transaction_id)).to.be.eq('70bad87b2799a237a1144cc6e898b2a29c3a27c0761cf7b9438d27d4d47a65af');
   });
-
 });
 
 function expect_uint8Array(bin1, bin2){
